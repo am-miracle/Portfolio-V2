0dcaa5c151f25e094e62fed25b921e330af2019a
Miracle Jude <judemiraco0@gmail.com>
2023-02-05 20:49:35 +0100

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,55 @@
-window.onscroll = function() {myFunction()};
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
 
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+const nav = document.getElementById('nav');
+
+
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let currentScrolledPosition = window.scrollY || window.pageYOffset;
+  let scrollingDown;
+
+  if (currentScrolledPosition > previousScrollPosition) {
+    scrollingDown = true;
   } else {
-    navbar.classList.remove("sticky");
+    scrollingDown = false;
+  }
+  previousScrollPosition = currentScrolledPosition;
+  return scrollingDown;
+};
+
+const handleNavScroll = () => {
+  if (isScrollingDown() && !nav.contains(document.activeElement)) {
+    nav.classList.add('scroll-down');
+    nav.classList.remove('scroll-up');
+  } else {
+    nav.classList.add('scroll-up');
+    nav.classList.remove('scroll-down')
   }
 }
+
+// initialize a throttleWait variable
+let throttleWait;
+
+const throttle = (callback, time) => {
+  // if the variable is true, don't run the function
+  if (throttleWait) return;
+
+  // set the wait variable to true to pause the function
+  throttleWait = true;
+
+  // use setTimeout to run the function within the specified time
+  setTimeout(() => {
+    callback();
+
+    // set throttleWait to false once the timer is up to restart the throttle function
+    throttleWait = false;
+  }, time);
+}
+
+window.addEventListener("scroll", () => {
+  if (mediaQuery && !mediaQuery.matches) {
+    throttle(handleNavScroll, 250)
+  }
+});
