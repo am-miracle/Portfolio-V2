48afe1e61975bbd505a2b427f96a0feddf6e9e62
Miracle Jude <judemiraco0@gmail.com>
2023-02-06 02:47:20 +0100

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,19 @@
 const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
 
-
 const nav = document.getElementById('nav');
+const navMenu =  document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close')
+const navLinks = document.querySelectorAll('.nav-link');
+//   const header = document.getElementById('header');
+const theme = document.querySelector("#theme-button");
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+const colorPalette = document.querySelectorAll('.choose-color span');
+var root = document.querySelector(":root");
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
 
 
 let previousScrollPosition = 0;
@@ -48,8 +60,192 @@ const throttle = (callback, time) => {
   }, time);
 }
 
+// SHOW MENU
+
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+// MENU HIDDEN
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+const linkAction = () => {
+  // when we click on each nav link, we remove the show menu class
+  navMenu.classList.remove('show-menu')
+}
+navLinks.forEach(link => link.addEventListener('click', linkAction))
+
+
+// change background header
+
+// const scrollHeader() {
+//   // when the scroll is greater than 80 viewport height, add the class scroll header to the tag header
+//   if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+// }
+
 window.addEventListener("scroll", () => {
   if (mediaQuery && !mediaQuery.matches) {
     throttle(handleNavScroll, 250)
   }
+  // scrollHeader()
 });
+
+// Scroll sections active link
+
+// get all sections that have an id defined
+const sections = document.querySelectorAll('section[id]');
+
+
+const navHighlighter = () => {
+  // get current scroll position
+  let scrollY = window.pageYOffset;
+  // now we loop through sections to get height , top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 40,
+    sectionId = current.getAttribute('id');
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector(`.nav-menu a[href*= "${sectionId}" ]`).classList.add("active-link")
+    }
+    else{
+      document.querySelector(`.nav-menu a[href*= "${sectionId}" ]`).classList.remove("active-link")
+    }
+  })
+}
+// add an event listener listening for scroll
+window.addEventListener('scroll', navHighlighter);
+
+// ==== Theme customization ===
+
+// open modal
+const openThemeModal = () => {
+  themeModal.style.display = 'grid'
+}
+// close modal
+const closeThemeModal = (e) => {
+  if(e.target.classList.contains('customize-theme')) {
+    themeModal.style.display = 'none'
+  }
+}
+theme.addEventListener('click', openThemeModal);
+themeModal.addEventListener('click', closeThemeModal);
+
+// Choose Fonts
+
+// remove active class from font size selectors
+const removeSizeSelector = () => {
+  fontSizes.forEach(size => {
+    size.classList.remove("active");
+  })
+}
+fontSizes.forEach(size => {
+  size.addEventListener('click', () => {
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle('active')
+    if(size.classList.contains('font-size-1')){
+      fontSize = '12px'
+    }
+    else if(size.classList.contains('font-size-1')){
+      fontSize = '14px'
+    }
+    else if(size.classList.contains('font-size-2')){
+      fontSize = '14px'
+    }
+    else if(size.classList.contains('font-size-3')){
+      fontSize = '16px'
+    }
+    else if(size.classList.contains('font-size-4')){
+      fontSize = '18px'
+    }
+
+    // change font size fo the root html element
+    document.querySelector('html').style.fontSize = fontSize
+  })
+})
+
+// choose color
+const changeActiveColorClass = () => {
+  colorPalette.forEach(colorPicker => {
+    colorPicker.classList.remove('active')
+  })
+}
+colorPalette.forEach(color => {
+  color.addEventListener('click', () => {
+    let primaryHue;
+    changeActiveColorClass();
+
+    if(color.classList.contains('color-1')){
+      primaryHue = 252;
+    }
+    else if(color.classList.contains('color-2')){
+      primaryHue = 52;
+    }
+    else if(color.classList.contains('color-3')){
+      primaryHue = 352
+    }
+    else if(color.classList.contains('color-4')){
+      primaryHue = 152
+    }
+    else if(color.classList.contains('color-5')){
+      primaryHue = 202
+    }
+    color.classList.add("active");
+    root.style.setProperty("--primary-color-hue", primaryHue)
+  })
+})
+
+// Theme backgrounds
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+// change background color
+const changeBG = () => {
+  root.style.setProperty('--light-color-lightness', lightColorLightness);
+  root.style.setProperty('--white-color-lightness', whiteColorLightness);
+  root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+Bg1.addEventListener('click', () => {
+  // add active class
+  Bg1.classList.add('active');
+  // remove active class from the others
+  Bg2.classList.remove('active');
+  Bg3.classList.remove('active');
+  // remove customized changes from local storage
+  window.location.reload();
+});
+
+Bg2.addEventListener('click', () => {
+  darkColorLightness = '95%';
+  whiteColorLightness = '20%';
+  lightColorLightness = '15%';
+
+  // add active class
+  Bg2.classList.add('active');
+  // remove active class from the others
+  Bg1.classList.remove('active');
+  Bg3.classList.remove('active');
+  changeBG();
+});
+
+Bg3.addEventListener('click', () => {
+  darkColorLightness = '95%';
+  whiteColorLightness = '10%';
+  lightColorLightness = '0%';
+
+  // add active class
+  Bg3.classList.add('active');
+  // remove active class from the others
+  Bg1.classList.remove('active');
+  Bg2.classList.remove('active');
+  changeBG();
+})
