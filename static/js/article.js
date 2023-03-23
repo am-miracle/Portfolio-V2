bb93a623285da55c8fcf4aecb4297dfdd908e8b6
Miracle Jude <judemiraco0@gmail.com>
2023-03-23 22:11:09 +0100

    udpate article.js

--- static/js/article.js
+++ static/js/article.js
@@ -1,29 +1,25 @@
 // articles filter
-filterSelection("all")
 const filterSelection = (c) => {
-  const x = document.getElementsByClassName("filterDiv");
-  let i;
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  const filterElement = document.getElementsByClassName("filterDiv");
+  if (c === "all") c = "";
+  for (let i = 0; i < filterElement.length; i++) {
+    removeClass(filterElement[i], "show");
+    if (filterElement[i].className.indexOf(c) > -1) addClass(filterElement[i], "show");
   }
 }
 
 const addClass = (element, name) => {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
+  const arr1 = element.className.split(" ");
+  const arr2 = name.split(" ");
+  for (let i = 0; i < arr2.length; i++) {
     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
 }
 
 const removeClass = (element, name) => {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
+  const arr1 = element.className.split(" ");
+  const arr2 = name.split(" ");
+  for (let i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
@@ -31,9 +27,11 @@ const removeClass = (element, name) => {
   element.className = arr1.join(" ");
 }
 
+filterSelection("all");
+
 const btnContainer = document.getElementById("filter-container");
 const btns = btnContainer.getElementsByClassName("filter-btn");
-for (var i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     const current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
